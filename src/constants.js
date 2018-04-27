dd5f6205131824fff2b04e0413748fcbf21bc5de
Stefan Uzunov <stefan.uzunov@scalefocus.com>
2018-04-27 17:10:23 +0300

    Update the SAS string

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://sparkvision.file.core.windows.net/sparkvision';
-const SAS = '?sv=2017-07-29&ss=f&srt=sco&sp=rlc&se=2018-05-03T20:00:21Z&st=2018-04-27T12:00:21Z&spr=https&sig=xIwxVDKTVEX25TEE3TG0o42XnsC9r139PO8BrwXGIJw%3D';
+const SAS = '?sv=2017-07-29&ss=f&srt=sco&sp=rl&se=2018-05-04T22:08:35Z&st=2018-04-27T14:08:35Z&spr=https&sig=V7pQIuOaQ2CCW2wAPpLhSG0FZdByuz1lBhAxFzcqfhA%3D';
 
 export const AZURE_FILE_STORAGE = {
   BASE_URL,
