863c24e1579ab19b39d7b6d4459731a7c6839f43
Stefan Uzunov <stefan.uzunov@scalefocus.com>
2018-04-30 01:29:31 +0300

    Update SAS

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://sparkvision.file.core.windows.net/sparkvision';
-const SAS = '?sv=2017-07-29&ss=f&srt=sco&sp=rl&se=2018-05-04T22:08:35Z&st=2018-04-27T14:08:35Z&spr=https&sig=V7pQIuOaQ2CCW2wAPpLhSG0FZdByuz1lBhAxFzcqfhA%3D';
+const SAS = '?sv=2017-07-29&ss=bf&srt=sco&sp=rl&se=2018-05-04T22:39:28Z&st=2018-04-29T14:39:28Z&spr=https&sig=oOBFYUuyTApnmMNLNBKzlP7VuqYgixs7NFzOPpF1B90%3D';
 
 export const AZURE_FILE_STORAGE = {
   BASE_URL,
   SAS
-}
+};
 
-export const PRODUCTS_XML_URL = BASE_URL + '/products.xml' + SAS
+export const PRODUCTS_XML_URL = BASE_URL + '/products.xml' + SAS;
